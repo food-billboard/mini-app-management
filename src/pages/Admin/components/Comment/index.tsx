4b66b12978f2626ca29c9d81396df7651478ca7d
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-01-17 10:12:09 +0800

    feat(): :necktie: 电影列表新增名称搜索

--- src/pages/Admin/components/Comment/index.tsx
+++ src/pages/Admin/components/Comment/index.tsx
@@ -1,77 +1,76 @@
-import { Pagination, List, Row, Col, Avatar, Image } from 'antd'
-import React, { memo, useCallback, useState, Fragment, useEffect, useMemo } from 'react'
-import { unstable_batchedUpdates } from 'react-dom'
-import { merge } from 'lodash'
-import { connect } from 'dva'
-import moment from 'moment'
-import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons'
-import { Preview } from '../../../Image'
-import { PreView as VideoPreView } from '../../../Video'
-import { mapStateToProps, mapDispatchToProps } from '../../connect'
-import { GerAdminCommentList } from '@/services'
-import { IMAGE_FALLBACK } from '@/utils'
-import styles from './index.less'
+import { Pagination, List, Row, Col, Avatar, Image } from 'antd';
+import React, { memo, useCallback, useState, Fragment, useEffect, useMemo } from 'react';
+import { unstable_batchedUpdates } from 'react-dom';
+import { merge } from 'lodash';
+import { connect } from 'dva';
+import moment from 'moment';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons';
+import { Preview } from '../../../Image';
+import { PreView as VideoPreView } from '../../../Video';
+import { mapStateToProps, mapDispatchToProps } from '../../connect';
+import { GerAdminCommentList } from '@/services';
+import { IMAGE_FALLBACK } from '@/utils';
+import styles from './index.less';
 
 interface IProps {
-  userInfo: API_ADMIN.IGetAdminInfoRes
+  userInfo: API_ADMIN.IGetAdminInfoRes;
 }
 
 const ICON_MAP = {
   star: StarOutlined,
   like: LikeOutlined,
-  message: MessageOutlined
-}
+  message: MessageOutlined,
+};
 
 const Comments = memo((props: IProps) => {
-
-  const [ list, setList ] = useState<API_ADMIN.IGetAdminCommentData[]>([])
-  const [ currPage, setCurrPage ] = useState<number>(1)
-  const [ total, setTotal ] = useState<number>(0)
+  const [list, setList] = useState<API_ADMIN.IGetAdminCommentData[]>([]);
+  const [currPage, setCurrPage] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
 
   const { userInfo } = useMemo(() => {
-    return props
-  }, [props])
+    return props;
+  }, [props]);
 
-  const IconText = ({ type, text }: { type: keyof typeof ICON_MAP, text: string }) => {
-
-    const Component = ICON_MAP[type]
+  const IconText = ({ type, text }: { type: keyof typeof ICON_MAP; text: string }) => {
+    const Component = ICON_MAP[type];
 
     return (
       <span>
-        <Component 
+        <Component
           style={{
             marginRight: 8,
           }}
         />
         {text}
       </span>
-    )
-  }
+    );
+  };
 
-  const fetchData = useCallback(async (params: API_ADMIN.IGetAdminCommentListParams={}) => {
-    const data = await GerAdminCommentList(merge({}, params, {
-      currPage: currPage - 1,
-      pageSize: 10
-    }) as API_ADMIN.IGetAdminCommentListParams)
-    unstable_batchedUpdates(() => {
-      setTotal(data.total || 0)
-      setList(data.list || [])
-    })
-  }, [currPage])
+  const fetchData = useCallback(
+    async (params: API_ADMIN.IGetAdminCommentListParams = {}) => {
+      const data = await GerAdminCommentList(
+        merge({}, params, {
+          currPage: currPage - 1,
+          pageSize: 10,
+        }) as API_ADMIN.IGetAdminCommentListParams,
+      );
+      unstable_batchedUpdates(() => {
+        setTotal(data.total || 0);
+        setList(data.list || []);
+      });
+    },
+    [currPage],
+  );
 
-  const renderMedia = useCallback((content: API_ADMIN.IGetAdminCommentData["content"]) => {
-    const { image=[], video=[] } = content || {}
-    const result = [...image.map(item => ({ src: item, video: false }))]
-    result.push(...video.map(item => ({ src: item, video: true })))
+  const renderMedia = useCallback((content: API_ADMIN.IGetAdminCommentData['content']) => {
+    const { image = [], video = [] } = content || {};
+    const result = [...image.map((item) => ({ src: item, video: false }))];
+    result.push(...video.map((item) => ({ src: item, video: true })));
     return result.map((item, index) => {
-      const { video: isVideo, src } = item 
-      const method = isVideo ? VideoPreView.bind(null, video) : Preview.bind(null, image)
+      const { video: isVideo, src } = item;
+      const method = isVideo ? VideoPreView.bind(null, video) : Preview.bind(null, image);
       return (
-        <Col 
-          key={index}
-          span={8}
-          onClick={() => method()}
-        >
+        <Col key={index} span={8} onClick={() => method()}>
           <Image
             fallback={IMAGE_FALLBACK}
             src={isVideo ? '' : src}
@@ -79,17 +78,17 @@ const Comments = memo((props: IProps) => {
             className={styles['media-image-list-item']}
           />
         </Col>
-      )
-    })
-  }, [])
+      );
+    });
+  }, []);
 
   const onPageChange = useCallback((page: number) => {
-    setCurrPage(page)
-  }, [])
+    setCurrPage(page);
+  }, []);
 
   useEffect(() => {
-    fetchData()
-  }, [currPage])
+    fetchData();
+  }, [currPage]);
 
   return (
     <Fragment>
@@ -99,9 +98,9 @@ const Comments = memo((props: IProps) => {
         rowKey="id"
         itemLayout="vertical"
         dataSource={list}
-        renderItem={item => (
+        renderItem={(item) => (
           <List.Item
-            key={item["_id"]}
+            key={item['_id']}
             actions={[
               // <IconText key="star" type="star" text={item.total_like} />,
               <IconText key="like" type="like" text={item.total_like.toString()} />,
@@ -111,19 +110,16 @@ const Comments = memo((props: IProps) => {
             <List.Item.Meta
               title={
                 <div className={styles.listItemMetaTitle}>
-                  <Avatar 
-                    src={userInfo.avatar}
-                    style={{marginRight: 20}}
-                  >{userInfo.username}</Avatar>
+                  <Avatar src={userInfo.avatar} style={{ marginRight: 20 }}>
+                    {userInfo.username}
+                  </Avatar>
                   {userInfo.username}
                 </div>
               }
               description={
                 <span>
                   {item.content?.text}
-                  <Row gutter={24}>
-                    {renderMedia(item.content)}
-                  </Row>
+                  <Row gutter={24}>{renderMedia(item.content)}</Row>
                 </span>
               }
             />
@@ -131,7 +127,7 @@ const Comments = memo((props: IProps) => {
           </List.Item>
         )}
       />
-      <div style={{textAlign: 'right'}}>
+      <div style={{ textAlign: 'right' }}>
         <Pagination
           pageSize={10}
           current={currPage}
@@ -141,7 +137,7 @@ const Comments = memo((props: IProps) => {
         />
       </div>
     </Fragment>
-  )
-})
+  );
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Comments)
+export default connect(mapStateToProps, mapDispatchToProps)(Comments);
--- src/pages/Data/columns.tsx
+++ src/pages/Data/columns.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { history } from 'umi'
-import { DatePicker } from 'antd'
-import moment from 'moment'
-import { SOURCE_TYPE, MOVIE_STATUS } from '@/utils'
+import React from 'react';
+import { history } from 'umi';
+import { DatePicker } from 'antd';
+import moment from 'moment';
+import { SOURCE_TYPE, MOVIE_STATUS } from '@/utils';
 
-const { RangePicker } = DatePicker
+const { RangePicker } = DatePicker;
 
 export default [
   {
@@ -15,19 +15,18 @@ export default [
   {
     title: '电影名称',
     dataIndex: 'name',
-    render: (value: string, record: API_DATA.IGetMovieData) => <a onClick={() => history.push(`/data/main/${record["_id"]}`)}>{value}</a>
+    render: (value: string, record: API_DATA.IGetMovieData) => (
+      <a onClick={() => history.push(`/data/main/${record['_id']}`)}>{value}</a>
+    ),
   },
   {
     title: '作者名称',
     dataIndex: 'author',
     valueType: 'option',
-    renderText: (value: {
-      username: string
-      _id: string
-    }) => {
-      if(!value.username) return (<span>-</span>)
-      return <a onClick={() => history.push(`/member/${value["_id"]}`)}>{value.username}</a>
-    }
+    renderText: (value: { username: string; _id: string }) => {
+      if (!value.username) return <span>-</span>;
+      return <a onClick={() => history.push(`/member/${value['_id']}`)}>{value.username}</a>;
+    },
   },
   {
     title: '创建时间',
@@ -36,9 +35,9 @@ export default [
     valueType: 'date',
     hideInSearch: true,
     renderFormItem: (_: any, { type, defaultRender, ...rest }: any) => {
-      return <RangePicker {...rest} />
+      return <RangePicker {...rest} />;
     },
-    renderText: (val: string) => moment(val).format('YYYY-MM-DD hh:mm:ss')
+    renderText: (val: string) => moment(val).format('YYYY-MM-DD hh:mm:ss'),
   },
   {
     title: '创建时间',
@@ -59,7 +58,7 @@ export default [
     dataIndex: 'updatedAt',
     sorter: true,
     hideInSearch: true,
-    renderText: (val: string) => moment(val).format('YYYY-MM-DD hh:mm:ss')
+    renderText: (val: string) => moment(val).format('YYYY-MM-DD hh:mm:ss'),
   },
   {
     title: '来源',
@@ -67,23 +66,22 @@ export default [
     valueEnum: Object.keys(SOURCE_TYPE).reduce((acc: any, cur: string) => {
       acc[cur] = {
         text: SOURCE_TYPE[cur],
-        source_type: cur
-      }
-      return acc
+        source_type: cur,
+      };
+      return acc;
     }, {}),
-    renderText: (val: string) => SOURCE_TYPE[val] || '-'
+    renderText: (val: string) => SOURCE_TYPE[val] || '-',
   },
   {
     title: '状态',
     dataIndex: 'status',
     valueEnum: Object.keys(MOVIE_STATUS).reduce((acc: any, cur: string) => {
-
       acc[cur] = {
         text: MOVIE_STATUS[cur],
-        status: cur
-      }
-      return acc
-    }, {})
+        status: cur,
+      };
+      return acc;
+    }, {}),
   },
   // {
   //   hideInTable: false,
@@ -93,4 +91,4 @@ export default [
   //     return <Input {...rest} />
   //   }
   // },
-]
+];
