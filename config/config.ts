09acd70fbc261e01f474cd4bb452286eae1dbc50
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-08-29 16:38:35 +0800

    refacotr(): :wrench: 打包优化

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,16 @@
 // https://umijs.org/config/
 import { defineConfig } from 'umi';
-import { merge } from 'lodash'
+import { merge } from 'lodash';
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
+// import HappyPack from 'happypack'
+// import os from 'os'
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routerConfig from './router-config';
+
+// const happyThreadPool = HappyPack.ThreadPool({
+//   size: os.cpus().length
+// })
 
 const { REACT_APP_ENV } = process.env;
 
@@ -49,6 +55,12 @@ const commonConfig = {
   manifest: {
     basePath: '/',
   },
+};
+
+const developmentConfig: any = merge({}, commonConfig, {
+  define: {
+    'process.env.REACT_APP_ENV': 'dev',
+  },
   chainWebpack(config: any) {
     config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
       {
@@ -56,23 +68,82 @@ const commonConfig = {
       },
     ]);
   },
-}
-
-const developmentConfig: any = merge({}, commonConfig, {
-  define: {
-    'process.env.REACT_APP_ENV': 'dev',
-  },
-})
+});
 
 const productionConfig: any = merge({}, commonConfig, {
   define: {
-    'process.env.REACT_APP_ENV': 'prod'
+    'process.env.REACT_APP_ENV': 'prod',
   },
+  chunks: ['antdesigns', 'vendors', 'commons', 'umi'],
   //-----打包配置
   base: '/api/backend/',
-  publicPath: "/api/backend/"
-})
+  publicPath: '/api/backend/',
+  chainWebpack(config: any) {
+    // config.plugin('HappyPack').use(HappyPack, [
+    //   {
+    //     id: 'js',
+    //     loaders: ['babel-loader'],
+    //     threadPool: happyThreadPool,
+    //   },
+    // ]);
 
-export default defineConfig(
-  REACT_APP_ENV === "prod" ? productionConfig : developmentConfig
-);
+    config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
+      {
+        languages: ['javascript', 'json'],
+      },
+    ]);
+    // 过滤掉momnet的那些不使用的国际化文件
+    config
+      .plugin('replace')
+      .use(require('webpack').ContextReplacementPlugin)
+      .tap(() => {
+        return [/moment[/\\]locale$/, /zh-cn/];
+      });
+
+    config.merge({
+      optimization: {
+        minimize: true,
+        splitChunks: {
+          chunks: 'async',
+          minSize: 30000,
+          minChunks: 1,
+          automaticNameDelimiter: '.',
+          cacheGroups: {
+            lfpantdesigns: {
+              name: 'antdesigns',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](@antv|antd|@ant-design)/,
+              priority: 10,
+            },
+            lfpvendors: {
+              name: 'vendors',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](lodash|moment|react|dva|postcss|mapbox-gl|video)/,
+              priority: 10,
+            },
+            // // 最基础的
+            // 'async-commons': {
+            //   // 其余异步加载包
+            //   name: 'async-commons',
+            //   chunks: 'async',
+            //   minChunks: 2,
+            //   priority: 2,
+            // },
+            lfpcommons: {
+              name: 'commons',
+              // 其余同步加载包
+              chunks: 'all',
+              minChunks: 2,
+              priority: 1,
+              // 这里需要注意下，webpack5会有问题， 需加上这个 enforce: true，
+              // refer: https://github.com/webpack-contrib/mini-css-extract-plugin/issues/257#issuecomment-432594711
+              enforce: true,
+            },
+          },
+        },
+      },
+    });
+  },
+});
+
+export default defineConfig(REACT_APP_ENV === 'prod' ? productionConfig : developmentConfig);
